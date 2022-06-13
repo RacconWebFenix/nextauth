aa80351e381a23e088f36c401a50e5225efe1136
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-06-13 11:45:06 -0300

    Realizando logout automático

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
-import { parseCookies, setCookie } from "nookies";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
+import { signOut } from "../context/AuthContext";
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -85,8 +86,9 @@ api.interceptors.response.use(
           });
         });
       } else {
-        //deslogar usuario
+        signOut();
       }
     }
+    return Promise.reject(error);
   }
 );
